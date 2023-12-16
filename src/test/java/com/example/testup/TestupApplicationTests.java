9807e364b7bf57660d4a31134c1facfdd9a5e160
hiikaruu <yukko.moriyama@gmail.com>
2023-12-16 13:05:25 +0300

    хуйня ебаная

--- src/test/java/com/example/testup/TestupApplicationTests.java
+++ src/test/java/com/example/testup/TestupApplicationTests.java
@@ -1,12 +1,5 @@
 package com.example.testup;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class TestupApplicationTests {
-
-	@Test
 	void contextLoads() {
 	}
 
